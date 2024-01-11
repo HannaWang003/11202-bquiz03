dbf412373fbe6c8fbeb37add68da3d1c2f94419e
Hanna <hannawang003@gmail.com>
2024-01-11 14:11:30 +0800

    完成預告片清單編輯功能

--- api/add_poster.php
+++ api/add_poster.php
@@ -3,10 +3,11 @@ include_once "db.php";
 if (isset($_FILES['poster']['tmp_name'])) {
     move_uploaded_file($_FILES['poster']['tmp_name'], "../img/{$_FILES['poster']['name']}");
     $_POST['img'] = $_FILES['poster']['name'];
+
+    $_POST['sh'] = 1;
+    $_POST['rank'] = $Poster->max('id') + 1;
+    $_POST['ani'] = rand(1, 3);
+    // dd($_POST);
+    $Poster->save($_POST);
 }
-$_POST['sh'] = 1;
-$_POST['rank'] = $Poster->max('id') + 1;
-$_POST['ani'] = rand(1, 3);
-// dd($_POST);
-$Poster->save($_POST);
 to("../back.php?do=poster");
--- back/poster.php
+++ back/poster.php
@@ -1,19 +1,20 @@
 <style>
-    .item {
-        display: flex;
-        padding: 2px;
-        margin: 3px;
-        justify-content: space-between;
-        align-items: center;
-    }
+.item {
+    display: flex;
+    padding: 2px;
+    margin: 3px;
+    justify-content: space-between;
+    align-items: center;
+    background: #ddd;
+}
 
-    .item {
-        div {
-            width: 24.5%;
-            margin: 0 0.25%;
-            text-align: center;
-        }
+.item {
+    div {
+        width: 24.5%;
+        margin: 0 0.25%;
+        text-align: center;
     }
+}
 </style>
 <div>
     <h3 class='ct'>預告片清單</h3>
@@ -29,6 +30,7 @@
     $pos = $Poster->all("order by `rank`");
     foreach ($pos as $idx => $po) {
     ?>
+    <form action="./api/edit_poster.php" method="post">
         <div class="item">
             <div><img src="./img/<?= $po['img'] ?>" alt="" style="width:60px;height:80px;"></div>
             <div><input type="text" name="name[]" id="" value=<?= $po['name'] ?>></div>
@@ -36,24 +38,27 @@
                 <input type="button" value="往上"><input type="button" value="往下">
             </div>
             <div>
+                <input type="hidden" name="id[]" value=<?= $po['id'] ?>>
                 <!-- input:checkbook*2+select>option*3 -->
-                <input type="checkbox" name="sh[]" id="" value=<?= $po['id'] ?> <?= ($po['sh'] == 1) ? 'check' : '' ?>>顯示
+                <input type="checkbox" name="sh[]" id="" value=<?= $po['id'] ?>
+                    <?= ($po['sh'] == 1) ? 'checked' : '' ?>>顯示
                 <input type="checkbox" name="del[]" id="" value=<?= $po['id'] ?>>刪除
-                <select name="ani" id="">
-                    <option value="1">淡入</option>
-                    <option value="2">縮放</option>
-                    <option value="3">滑入滑出</option>
+                <select name="ani[]" id="">
+                    <option value="1" <?= ($po['ani'] == 1) ? 'selected' : '' ?>>淡入淡出</option>
+                    <option value="2" <?= ($po['ani'] == 2) ? 'selected' : '' ?>>縮放</option>
+                    <option value="3" <?= ($po['ani'] == 3) ? 'selected' : '' ?>>滑入滑出</option>
                 </select>
             </div>
         </div>
-    <?php
+        <?php
     }
-    ?>
+        ?>
 </div>
 <div class="ct">
     <input type="submit" value="編輯確定">
     <input type="reset" value="重置">
 </div>
+</form>
 <hr>
 <div>
     <h3 class='ct'>新增預告片海報</h3>
