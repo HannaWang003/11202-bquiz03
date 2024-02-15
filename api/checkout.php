2f2ff0480ecf7a8cb7e12b122e75b65be5ab10a3
Hanna <hannawang003@gmail.com>
2024-02-15 10:18:53 +0800

    update_20240215

--- api/checkout.php
+++ api/checkout.php
@@ -1,9 +1,8 @@
-<?php
-include_once "db.php";
+<?php include_once "db.php";
+
 sort($_POST['seats']);
 $_POST['seats'] = serialize($_POST['seats']);
 $id = $Order->max('id') + 1;
-$_POST['no'] = date('Ymd') . sprintf("%04d", $id);
+$_POST['no'] = date("Ymd") . sprintf("%04d", $id);
 $Order->save($_POST);
 echo $_POST['no'];
-echo $no;
--- back/result.php
+++ back/result.php
@@ -3,26 +3,23 @@ $order = $Order->find(['no' => $_GET['no']]);
 ?>
 <table>
     <tr>
-        <td colspan=2>感謝您的訂購，你的訂單編號是:
-            <? $_GET['no'] ?>
-        </td>
-        <!-- <td></td> -->
+        <td colspan='2'>感謝您的訂購，您的訂單編號是:<?= $_GET['no']; ?></td>
     </tr>
     <tr>
-        <td>電影名稱</td>
-        <td><?= $order['movie'] ?></td>
+        <td>電影名稱：</td>
+        <td><?= $order['movie']; ?></td>
     </tr>
     <tr>
-        <td>日期</td>
-        <td><?= $order['daate'] ?></td>
+        <td>日期：</td>
+        <td><?= $order['date']; ?></td>
     </tr>
     <tr>
-        <td>場次時間</td>
-        <td><?= $order['session'] ?></td>
+        <td>場次時間：</td>
+        <td><?= $order['session']; ?></td>
     </tr>
     <tr>
         <td colspan='2'>
-            座位:<br>
+            座位：<br>
             <?php
             $seats = unserialize($order['seats']);
             foreach ($seats as $seat) {
@@ -30,11 +27,9 @@ $order = $Order->find(['no' => $_GET['no']]);
                 echo (($seat % 5) + 1) . "號";
                 echo "<br>";
             }
-            echo "共{$order['qt']}張電影片";
-
+            echo "共{$order['qt']}張電影票"
             ?>
         </td>
-        <!-- <td></td> -->
     </tr>
 </table>
-<div class="ct"><button onclick="location.href='index.php'"></button></div>
+<div class="ct"><button onclick="location.href='index.php'">確認</button></div>
